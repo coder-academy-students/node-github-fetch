aea6b4a3911900713343226ca4ab7e559210979c
Patrick Smith <patrick@burntcaramel.com>
2016-12-12 17:14:26 +1100

    added comments

--- db/db.js
+++ db/db.js
@@ -1,26 +1,38 @@
+/*
+An example of how a database driver would behave
+Putting the database code here instead of the routes means
+you can "do it once" and call it anywhere (and have only one place to fix)
+This makes it easier for you and keeps it DRY
+*/
+
 const fetch = require('node-fetch')
 const uri = 'https://raw.githubusercontent.com/coder-factory-academy/jsondb/master/db.json'
 
+//function that when run returns a promise of users
 function getUsers() {
  return fetch(uri).then(function(res) {
      return res.json()
   })
 }
 
+//function that when run returns a promise with ONE user
 function getOneUser(id) {
-  console.log(id)
   return fetch(uri).then(function(res){
     return res.json()
   })
   .then(function(users){
-
+    //loop through the array of users until you find the one you want
+    //forEach will map all where as find stop once it finds a match
     const user = users.find(function(user) {
-    if(user.id === id){
-        console.log("match")
-        return user;
-    }});
+        //condition to match
+        if(user.id === id){
+            return user;
+        }
+    });
+    //finally return the found user and it will be wrapped in a promise by the .then()
     return user
   })
 }
 
+//export both functions so they can be run elsewhere
 module.exports = { getUsers, getOneUser}
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,21 @@
 var express = require('express');
 var router = express.Router();
+//pull in the two functions that you can run to get data
 const {getUsers, getOneUser} = require('../db/db')
+
+//code commented out gets users from json as object
 // const Users = require('../db/users')
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   // const users = Users()
-  getUsers() // -> promise fetch(uri) -> results
+  getUsers() // -> promise fetch(uri) -> results -> users
   .then(function(users) {
     res.render('users/index', {
       title: 'Users',
       users: users
     })
-  })
+  }) //catch errors from ANY promise
   .catch(function(err){
     res.status(404).json({
       err: err.message
@@ -21,19 +25,25 @@ router.get('/', function(req, res, next) {
   // res.send('respond with a resource');
 });
 
+// :id will be whatever (1 or 2 or 3 etc.)
 router.get('/show/:id', function(req, res){
+  //decontruct the params to get id from the url
   const {id} = req.params
 
-  getOneUser(id)
+  getOneUser(id) // gets one user returned as promise
   .then(function(user) {
+    //if user undefined
     if(!user) {
       res.send(404).json({err: "User not found!"});
     }
+
+    //render out users page
     res.render('users/show', {
       title: `About ${user.name}`,
       user
     })
   })
+  //catch any error in any promise (one catch all)
   .catch(function(err){
     res.status(404).json({
       err: err.message
@@ -42,6 +52,5 @@ router.get('/show/:id', function(req, res){
 
 
 })
-
 
 module.exports = router;
